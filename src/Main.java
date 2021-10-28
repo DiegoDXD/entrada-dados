a4dd51ccf18a3f5b394d06f4e73016c4c5e7baa8
Diego Melo <diego-melo2020@outlook.com>
2021-10-28 20:58:01 -0300

    Projeto criado

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	//Ex: Digitando String
+	//Scanner sc = new Scanner(System.in);
+	//String x;
+    //x = sc.next();
+	//System.out.println("Voce digitou: " + x);
+	//sc.close();
+		
+	//Ex: Digitando um numero Int
+	//Scanner sc = new Scanner(System.in);
+	//int x;
+	//x = sc.nextInt();
+	//System.out.println("Voce digitou: " + x);
+	//sc.close();
+		
+	//Ex: Ditando um numero double com ponto
+	//Scanner sc = new Scanner(System.in);
+	//double x;
+	//x = sc.nextDouble();
+	//System.out.println("Voce digitou: " + x);
+	//sc.close();
+	
+	//Ex: Ditando um numero double com "printf"
+	//Scanner sc = new Scanner(System.in);
+	//double x;
+	//x = sc.nextDouble();
+	//System.out.printf("Voce digitou: %.2f%n", x);
+	//sc.close();
+		
+	//Ex: Digitando com ponto ao inves de virgula
+	//Locale.setDefault(Locale.US);
+	//Scanner sc = new Scanner(System.in);	
+	//double x;
+	//x = sc.nextDouble();
+	//System.out.printf("Voce digitou: %.2f%n", x);
+	//sc.close();
+	
+	//Ex: Digitando um caractere
+	//Locale.setDefault(Locale.US);
+	//Scanner sc = new Scanner(System.in);
+	//char x;
+	//x = sc.next().charAt(0);
+	//System.out.println("Voce digitou: " + x);	
+	//sc.close();
+	
+	//Ex: ler varios dados na mesma linha
+	//Locale.setDefault(Locale.US);
+	//Scanner sc = new Scanner(System.in);
+	//String x;
+	//int y;
+	//double z;
+	//x = sc.next();
+	//y = sc.nextInt();
+	//z = sc.nextDouble();
+	//System.out.println("Dados digitados:");
+	//System.out.println(x);
+	//System.out.println(y);
+	//System.out.println(z);
+		
+	}
+}
